4a9361479e7f9cd1f23d655bdadf790ba2c1881c
Suvam Bhattacharjee <suvambhat99@gmail.com>
2026-03-05 10:29:22 +0530

    loans microservie code completed requirs through testing

---
+++ loans/src/test/java/com/suvambhat99/loans/LoansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suvambhat99.loans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
