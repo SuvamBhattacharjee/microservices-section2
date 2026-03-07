81e003d13b5c636ab38d0dd08fdeccf3456aef27
Suvam Bhattacharjee <suvambhat99@gmail.com>
2026-03-07 19:53:15 +0530

    Cards microservice completed and tested

--- cards/cards/src/main/java/com/suvambhat99/Cards/repository/CardsRepository.java
+++ cards/cards/src/main/java/com/suvambhat99/Cards/repository/CardsRepository.java
@@ -1,4 +1,14 @@
 package com.suvambhat99.Cards.repository;
 
-public interface CardsRepository {
+import com.suvambhat99.Cards.entity.Cards;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CardsRepository extends JpaRepository<Cards, Long> {
+
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+
+    Optional<Cards> findByCardNumber(String cardNumber);
+
 }
